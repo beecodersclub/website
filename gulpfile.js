cd52891bde6f360d5bde5c90b593fc29bb3993b8
Luiz Mesquita <luiz.mesquita@pivot88.com>
2023-06-19 18:40:40 +0100

    feat(images): add WebP format support to images and resize profiles images
    This commit adds support for the WebP image format to the website's images.
    The images were converted to WebP format using the gulp-webp plugin.
    Additionally, the profiles images were resized to 256x256 pixels using the
    gulp-image-resize plugin.

--- gulpfile.js
+++ gulpfile.js
@@ -12,7 +12,7 @@ gulp.task('imagemin', function () {
         .pipe(gulp.dest('docs/images')); // Destination directory for optimized images
 });
 
-gulp.task('resize-and-convert', function () {
+gulp.task('resize-profiles', function () {
     return gulp.src('docs/images/profiles/*.{png,jpg,jpeg}')
         .pipe(imageResize({
             width: 256,
@@ -20,13 +20,18 @@ gulp.task('resize-and-convert', function () {
             crop: false,
             upscale: false
         }))
+        .pipe(gulp.dest('docs/images/profiles'));
+});
+
+gulp.task('webp', function () {
+    return gulp.src('docs/images/**/*.{png,jpg,jpeg}')
         .pipe(webp())
-        .pipe(gulp.dest('docs/images/profiles'))
+        .pipe(gulp.dest('docs/images'))
         .on('end', function () {
             // Excluir as imagens originais após a conversão para WebP
-            del(['docs/images/profiles/*.{png,jpg,jpeg}']);
+            del(['docs/images/**/*.{png,jpg,jpeg}']);
         });;
 });
 
 
-gulp.task('default', gulp.series('imagemin', 'resize-and-convert'));
+gulp.task('default', gulp.series('imagemin', 'resize-profiles', 'webp'));
