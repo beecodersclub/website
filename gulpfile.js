ccdea5fc20cddb74dc96549c85e35662187bd0df
Luiz Mesquita <luiz.mesquita@pivot88.com>
2023-06-19 18:14:23 +0100

    chore(profiles): update profile images to webp format and remove old jpg files
    chore(index.html): update team members' profile pictures and number of countries
    
    feat(gulpfile.js): add task to resize and convert images to WebP format
    feat(gulpfile.js): add del dependency to remove original images after conversion
    feat(package.json): add dependencies for image resizing and conversion to WebP format

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,32 @@
 const gulp = require('gulp');
 const imagemin = require('gulp-imagemin');
 const newer = require('gulp-newer');
+const imageResize = require('gulp-image-resize');
+const webp = require('gulp-webp');
+const del = require('del');
 
 gulp.task('imagemin', function () {
     return gulp.src('images/**/*') // Source directory of your images
-        .pipe(newer('dist/images')) // Only process newer images
+        // .pipe(newer('dist/images')) // Only process newer images
         .pipe(imagemin()) // Optimize the images
         .pipe(gulp.dest('docs/images')); // Destination directory for optimized images
 });
+
+gulp.task('resize-and-convert', function () {
+    return gulp.src('docs/images/profiles/*.{png,jpg,jpeg}')
+        .pipe(imageResize({
+            width: 256,
+            height: 256,
+            crop: false,
+            upscale: false
+        }))
+        .pipe(webp())
+        .pipe(gulp.dest('docs/images/profiles'))
+        .on('end', function () {
+            // Excluir as imagens originais após a conversão para WebP
+            del(['docs/images/profiles/*.{png,jpg,jpeg}']);
+        });;
+});
+
+
+gulp.task('default', gulp.series('imagemin', 'resize-and-convert'));
